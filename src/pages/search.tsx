db5d6b0b341a0253b9ce94e8b6fe3cafe9598bfc
Martin Sione <martinsione03@gmail.com>
2022-04-02 17:37:23 -0300

    Add sorting

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -5,13 +5,14 @@ import Link from "next/link";
 import axios from "axios";
 import HelperLupaMeli from "@components/HelperLupaMeli";
 import {
-  Stack,
   Box,
   Image,
+  Link as ChakraLink,
+  ListItem,
+  Select,
+  Stack,
   Text,
   UnorderedList,
-  ListItem,
-  Link as ChakraLink,
 } from "@chakra-ui/react";
 
 export default function Search() {
@@ -73,98 +74,115 @@ export default function Search() {
   }
 
   return (
-    <Stack direction="row">
-      <Stack maxW={292} minW="292">
-        <Stack direction="row">
-          <Stack
-            alignItems="center"
-            color="gray.600"
-            direction="row"
-            divider={<FiChevronRight />}
-            flexWrap="wrap"
-          >
-            {data.filters[0].values[0].path_from_root.map((filter: any) => (
-              <Text fontSize="14px">{filter.name} </Text>
+    <Stack spacing="24px">
+      <Stack direction="row" justifyContent="space-between">
+        <Stack
+          alignItems="center"
+          color="gray.600"
+          direction="row"
+          divider={<FiChevronRight />}
+          flexWrap="wrap"
+          maxW={292}
+          minW="292"
+        >
+          {data.filters[0].values[0].path_from_root.map((filter: any) => (
+            <Text fontSize="14px">{filter.name} </Text>
+          ))}
+        </Stack>
+        <Stack alignItems="center" direction="row" spacing="0">
+          <Text fontSize="14px" fontWeight="bold" whiteSpace="nowrap">
+            Ordenar por
+          </Text>
+          <Select _focus={{ outline: "none" }} border="none" fontSize="14px">
+            <option value={data.sort.id}>{data.sort.name}</option>
+            {data.available_sorts.map((filter: any) => (
+              <option value={filter.id}>{filter.name}</option>
+            ))}
+          </Select>
+        </Stack>
+      </Stack>
+      <Stack direction="row">
+        <Stack maxW={292} minW="292">
+          <Box>
+            <Text
+              color="gray.800"
+              fontSize={26}
+              fontWeight="medium"
+              textTransform="capitalize"
+            >
+              {query}
+            </Text>
+            <Text color="gray.600" fontSize="14px" fontWeight="thin">
+              {data.paging.total} resultados
+            </Text>
+          </Box>
+          <Stack spacing={6}>
+            {data.available_filters.map((filter: any) => (
+              <Box key={filter.id}>
+                <Text color="gray.800" fontSize="16px" fontWeight="medium">
+                  {filter.name}
+                </Text>
+                <Stack mt={2} spacing={2}>
+                  {filter.values.map((value: any) => (
+                    <Text key={value.id} color="gray.600" fontSize="14px">
+                      {value.name} ({value.results})
+                    </Text>
+                  ))}
+                </Stack>
+              </Box>
             ))}
           </Stack>
         </Stack>
-        <Box>
-          <Text
-            color="gray.800"
-            fontSize={26}
-            fontWeight="medium"
-            textTransform="capitalize"
-          >
-            {query}
-          </Text>
-          <Text color="gray.600" fontSize="14px" fontWeight="thin">
-            {data.paging.total} resultados
-          </Text>
-        </Box>
-        <Stack spacing={6}>
-          {data.available_filters.map((filter: any) => (
-            <Box key={filter.id}>
-              <Text color="gray.800" fontSize="16px" fontWeight="medium">
-                {filter.name}
-              </Text>
-              <Stack mt={2} spacing={2}>
-                {filter.values.map((value: any) => (
-                  <Text key={value.id} color="gray.600" fontSize="14px">
-                    {value.name} ({value.results})
-                  </Text>
-                ))}
+        <Stack
+          borderRadius="4px"
+          divider={<Box borderColor="gray.200" />}
+          flexGrow={1}
+          overflow="hidden"
+          shadow="md"
+          spacing={0}
+        >
+          {data.results.map((item: any) => (
+            <Stack key={item.id} bg="white" direction="row" p="6">
+              <Link href={`/product/${item.id}`}>
+                <a>
+                  <Stack h="160px" objectFit="contain" w="160px">
+                    <Image
+                      alt={item.title}
+                      m="auto"
+                      maxH="100%"
+                      maxW="100%"
+                      src={`https://http2.mlstatic.com/D_NQ_NP_${item.thumbnail_id}-V.webp`}
+                    />
+                  </Stack>
+                </a>
+              </Link>
+              <Stack>
+                <Link href={`/product/${item.id}`}>
+                  <a>
+                    <Text fontSize="20px" fontWeight="thin">
+                      {item.title}
+                    </Text>
+                  </a>
+                </Link>
+
+                <Link href={`/product/${item.id}`}>
+                  <a>
+                    <Text fontSize="24px">
+                      {item.price.toLocaleString("es-ar", {
+                        style: "currency",
+                        currency: "ARS",
+                        maximumFractionDigits: 0,
+                      })}
+                    </Text>
+                  </a>
+                </Link>
+                <Text color="whatsapp.600" fontSize="14px" fontWeight="bold">
+                  {item.shipping.free_shipping ? "Envio gratis" : ""}
+                </Text>
               </Stack>
-            </Box>
+            </Stack>
           ))}
         </Stack>
-      </Stack>
-      <Stack
-        borderRadius="3xl"
-        divider={<Box borderColor="gray.200" />}
-        flexGrow={1}
-        spacing={0}
-      >
-        {data.results.map((item: any) => (
-          <Stack key={item.id} bg="white" direction="row" p="6">
-            <Link href={`/product/${item.id}`}>
-              <a>
-                <Stack h="160px" objectFit="contain" w="160px">
-                  <Image
-                    alt={item.title}
-                    m="auto"
-                    maxH="100%"
-                    maxW="100%"
-                    src={`https://http2.mlstatic.com/D_NQ_NP_${item.thumbnail_id}-V.webp`}
-                  />
-                </Stack>
-              </a>
-            </Link>
-            <Stack>
-              <Link href={`/product/${item.id}`}>
-                <a>
-                  <Text fontSize="20px" fontWeight="thin">
-                    {item.title}
-                  </Text>
-                </a>
-              </Link>
-
-              <Link href={`/product/${item.id}`}>
-                <a>
-                  <Text fontSize="24px">
-                    {item.price.toLocaleString("es-ar", {
-                      style: "currency",
-                      currency: "ARS",
-                      maximumFractionDigits: 0,
-                    })}
-                  </Text>
-                </a>
-              </Link>
-              <Text color="whatsapp.600" fontSize="14px" fontWeight="bold">
-                {item.shipping.free_shipping ? "Envio gratis" : ""}
-              </Text>
-            </Stack>
-          </Stack>
-        ))}
       </Stack>
     </Stack>
   );
