fc8a1f23a6899c296ae256ae321f609564b99b11
Martin Sione <martinsione03@gmail.com>
2022-04-02 00:58:50 -0300

    Inital setup

---
+++ src/pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link href="/favicon.svg" rel="shortcut icon" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
