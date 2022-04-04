4b3f85d532b0de4100d378ce81ea3674da116966
Martin Sione <martinsione03@gmail.com>
2022-04-04 12:58:33 -0300

    Fix "RangeError: maximumFractionDigits value is out of range."

--- src/lib/formatPrice.ts
+++ src/lib/formatPrice.ts
@@ -6,6 +6,7 @@ export default function formatPrice(
   return price.toLocaleString(locale, {
     style: "currency",
     currency,
+    minimumFractionDigits: 0,
     maximumFractionDigits: 0,
   });
 }
